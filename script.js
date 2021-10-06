76d5e8137ee3964fec4347080496fa0823b9f6f0
SebaR <riolfosebastian@gmail.com>
2021-10-06 00:00:42 -0300

    ejercitacion js

---
+++ script.js
@@ -0,0 +1,51 @@
+// Ejercicio 3 
+
+/* let numero1 = parseInt (prompt("Ingresa un numero para sumar"))
+let numero2 = parseInt (prompt("Ingresa un número"))
+let resultado = parseInt(numero1 + numero2)
+ */
+/* console.log (resultado)
+document.write ('El resultado es ' + resultado)
+ */
+// Ejercicio 4
+
+/* let numero = parseInt (prompt("Ingrese su valor a calcular"))
+let numeroTriple = (numero * 3)
+let numeroElevado = (numero **3)
+
+document.write ('El resultado de la multiplicacion es ' + numeroElevado)
+document.write ("<br>")
+document.write ('El resultado de la multiplicacion es ' + numeroTriple)
+console.log (numeroTriple) 
+ */
+
+//Ejercicio 5
+
+/* let r = parseInt (5)
+let s = parseInt (10)
+let t = "Soy t, "
+
+document.write (r + s)
+document.write ("<br>")
+document.write (t + r) */
+
+//Ejercicio 6
+
+/* let texto = prompt("Ingrese un texto")
+
+alert ("El largo del texto ingresado es : " + texto.length)
+ */
+
+//Lo que hizo Julio
+
+let texto
+
+let ingreso = prompt('Ingrese un texto ')
+
+result = ingreso.length
+
+alert (result)
+
+numeros = result + 10
+
+document.write (numeros)
